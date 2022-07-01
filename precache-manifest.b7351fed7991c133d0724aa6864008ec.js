8856597c2073ae188dc9f4606ca69b951ce10471
yogeshjangid <yogeshrj1433@gmail.com>
2022-07-01 14:10:13 +0530

    Updates

---
+++ precache-manifest.b7351fed7991c133d0724aa6864008ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e57c03f4affeef0f9287",
+    "url": "/contactmanager/static/css/main.f6e80481.chunk.css"
+  },
+  {
+    "revision": "e57c03f4affeef0f9287",
+    "url": "/contactmanager/static/js/main.e57c03f4.chunk.js"
+  },
+  {
+    "revision": "2b447e1be3e7dd9deafc",
+    "url": "/contactmanager/static/js/1.2b447e1b.chunk.js"
+  },
+  {
+    "revision": "b5858ec4d71108196cb7",
+    "url": "/contactmanager/static/css/2.2163c99e.chunk.css"
+  },
+  {
+    "revision": "b5858ec4d71108196cb7",
+    "url": "/contactmanager/static/js/2.b5858ec4.chunk.js"
+  },
+  {
+    "revision": "d8368e4493b9785986ae",
+    "url": "/contactmanager/static/js/runtime~main.d8368e44.js"
+  },
+  {
+    "revision": "52b12c948ef3590594833c51cb87dcdb",
+    "url": "/contactmanager/index.html"
+  }
+];
