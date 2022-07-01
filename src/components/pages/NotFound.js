6d773c8d083111b0f78e93dcf805d545cab09051
yogeshjangid <yogeshrj1433@gmail.com>
2022-07-01 14:03:27 +0530

    initial commit

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div><h1 className='display-4'>404 Page Not Found</h1>
+    <p className="lead">Sorry ,That Page does not exist.</p>
+    </div>
+  );
+};
